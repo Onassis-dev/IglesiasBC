69e488f95ad26e599b122d5946df705861124ee5
Onassis <onassis.dev@gmail.com>
2025-04-08 16:41:42 -0700

    fix(app): Change user form from dialog to sheet

--- apps/app/src/pages/Settings/UserForm.tsx
+++ apps/app/src/pages/Settings/UserForm.tsx
@@ -1,11 +1,10 @@
 import { api, tsr } from '@/lib/boilerplate';
-import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Sheet, SheetContent, SheetBody } from '@/components/ui/sheet';
 import type { z } from 'zod';
-import { DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { SheetHeader, SheetTitle } from '@/components/ui/sheet';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useEffect } from 'react';
 import { showPromise } from '@/lib/showFunctions.tsx';
-import { Button } from '@/components/ui/button';
 import { Switch } from '@/components/ui/switch';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -55,157 +54,160 @@ const UserForm = ({ user, open, setOpen }: props) => {
     }, [user, open]);
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent className="max-w-4xl">
-                <DialogHeader>
-                    <DialogTitle>Actualizar usuario</DialogTitle>
-                </DialogHeader>
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetContent
+                className="max-w-4xl"
+                onSubmit={userForm.handleSubmit((values: z.infer<typeof EditPermissionSchema>) =>
+                    showPromise(handleSubmit(values), 'Información actualizada')
+                )}
+            >
+                <SheetHeader>
+                    <SheetTitle>Actualizar usuario</SheetTitle>
+                </SheetHeader>
 
-                <Form {...userForm}>
-                    <form
-                        onSubmit={userForm.handleSubmit((values: z.infer<typeof EditPermissionSchema>) =>
-                            showPromise(handleSubmit(values), 'Información actualizada')
-                        )}
-                        className="grid md:grid-cols-2 gap-x-12"
-                    >
-                        <FormField
-                            control={userForm.control}
-                            name="perm_members"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de miembros</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Controla el acceso y gestión de los miembros de tu iglesia.
-                                        </span>
-                                    </div>
-
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
+                <SheetBody>
+                    <Form {...userForm}>
+                        <form
+                            onSubmit={userForm.handleSubmit((values: z.infer<typeof EditPermissionSchema>) =>
+                                showPromise(handleSubmit(values), 'Información actualizada')
                             )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_inventory"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de inventarios</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Permite gestionar los inventarios y recursos disponibles.
-                                        </span>
-                                    </div>
+                        >
+                            <FormField
+                                control={userForm.control}
+                                name="perm_members"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de miembros</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Controla el acceso y gestión de los miembros de tu iglesia.
+                                            </span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_certificates"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de certificados</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Permite crear y gestionar los certificados de la iglesia.
-                                        </span>
-                                    </div>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_inventory"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de inventarios</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Permite gestionar los inventarios y recursos disponibles.
+                                            </span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_blog"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de Blog</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Permite gestionar y publicar contenido en el blog de la iglesia.
-                                        </span>
-                                    </div>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_certificates"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de certificados</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Permite crear y gestionar los certificados de la iglesia.
+                                            </span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_finances"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de finanzas</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">Controla la gestión financiera y los reportes.</span>
-                                    </div>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_blog"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de Blog</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Permite gestionar y publicar contenido en el blog de la iglesia.
+                                            </span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_website"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de página</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Permite gestionar el contenido y diseño de la página web de la iglesia.
-                                        </span>
-                                    </div>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_finances"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de finanzas</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">Controla la gestión financiera y los reportes.</span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={userForm.control}
-                            name="perm_presentations"
-                            render={({ field }) => (
-                                <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
-                                    <div className="grid">
-                                        <FormLabel className="font-semibold text-md">Permiso de presentaciones</FormLabel>
-                                        <span className="text-secondary font-medium text-xs">
-                                            Permite gestionar las presentaciones de la iglesia.
-                                        </span>
-                                    </div>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_website"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de página</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Permite gestionar el contenido y diseño de la página web de la iglesia.
+                                            </span>
+                                        </div>
 
-                                    <FormControl>
-                                        <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button className="w-48 ml-auto md:col-span-2" type="submit">
-                            Guardar
-                        </Button>
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={userForm.control}
+                                name="perm_presentations"
+                                render={({ field }) => (
+                                    <FormItem className="grid grid-cols-[auto,2.5rem] gap-4">
+                                        <div className="grid">
+                                            <FormLabel className="mb-2">Permiso de presentaciones</FormLabel>
+                                            <span className="text-secondary font-medium text-xs">
+                                                Permite gestionar las presentaciones de la iglesia.
+                                            </span>
+                                        </div>
+
+                                        <FormControl>
+                                            <Switch className="translate-y-1" checked={field.value} onCheckedChange={field.onChange} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </SheetBody>
+            </SheetContent>
+        </Sheet>
     );
 };
 
