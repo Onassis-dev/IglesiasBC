7fbc1e853572a0b927cdba5dc7bc6d1c21e8f913
Onassis <onassis.dev@gmail.com>
2025-05-19 10:19:43 -0700

    fix: Add OPTIONS method to CORS configuration for improved API compatibility

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   //protection
   app.enableCors({
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Disposition'],
     credentials: true,
